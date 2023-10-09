33b02d0fbf2d4b59a4320a76b5015a8b23909de3
caizelun <caizelun@bytedance.com>
2023-10-09 16:18:04 +0800

    feat: add prompt and data retrieve

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const telegram_token = process.env.BOT_TOKEN;
 const openai_token = process.env.OPENAI_TOKEN;
 
 if (!telegram_token || !openai_token) {
-  throw new Error('telegram/openai token not set');
+  throw new Error("telegram/openai token not set");
 }
 
 const bot = new TelegramBot(telegram_token, { polling: true });
@@ -16,7 +16,7 @@ const openai = new OpenAI({
   apiKey: openai_token,
 });
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer((req, res) => {
   switch (req.method) {
     case "GET":
       res.statusCode = 200;
@@ -24,41 +24,51 @@ const server = http.createServer(async (req, res) => {
       return;
 
     case "POST":
-      try {
-        const chat = await openai.chat.completions.create({
-          messages: [{ role: "user", content: "hi" }],
-          model: "gpt-3.5-turbo",
+      let body = "";
+      req
+        .on("data", (chunk) => {
+          body += chunk;
+        })
+        .on("end", async () => {
+          const chat = await openai.chat.completions.create({
+            messages: [
+              {
+                role: "user",
+                content: `你是一位精通日语的翻译，请将这段日语文字翻译为中文：\n${body}`,
+              },
+            ],
+            model: "gpt-3.5-turbo",
+          });
+
+          const text = chat.choices[0].message.content;
+          if (!text) {
+            throw new Error("no text returned");
+          }
+
+          console.log(JSON.stringify(chat.choices[0]));
+
+          // send message
+          if (lastId) {
+            bot.sendMessage(lastId, text);
+          }
+
+          // write response
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          res.write(text);
+          res.end();
+          return;
+        })
+        .on("error", (err: any) => {
+          console.log(err);
+          return;
         });
-
-        console.log(chat.choices);
-
-        const text = chat.choices[0].message.content;
-        if (!text) {
-          throw new Error("no text returned");
-        }
-
-        // send message
-        if (lastId) {
-          bot.sendMessage(lastId, text);
-        }
-
-        // write response
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        res.write(text);
-        res.end();
-        return;
-      } catch (err: any) {
-        console.log(err);
-        break;
-      }
-
+      break;
     default:
+      res.statusCode = 400;
+      res.end();
       break;
   }
-
-  res.statusCode = 400;
-  res.end();
 });
 
 bot.onText(/\/help/, (msg) => {
@@ -72,7 +82,7 @@ bot.onText(/\/start/, (msg) => {
 
   if (!server.listening) {
     server.listen(process.env.PORT, () => {
-      console.log(`Server running at 0.0.0.0:${process.env.PORT}`);
+      console.log(`Server running at port: ${process.env.PORT}`);
     });
   }
 });
